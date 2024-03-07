ba140ded6b8b145037d673c1a665d44a91254352
SilverFlin <toledorusso@outlook.com>
2024-03-06 18:18:31 -0700

    fix: quitar validacion incorrecta

--- src/pedido/input-dtos/crear-pedido-dto.ts
+++ src/pedido/input-dtos/crear-pedido-dto.ts
@@ -1,8 +1,6 @@
-import { IsString } from 'class-validator';
 import { Producto } from '../pedido.entity';
 
 export class CrearPedidoDTO {
-  @IsString()
   public productos: Producto[];
 
   constructor(productos: Producto[]) {
