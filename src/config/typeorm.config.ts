235629d27399389f94e01a5cc364497906973343
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2024-03-08 15:33:29 -0700

    ajuste

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,8 +5,8 @@ export const TypeOrmOptions: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: '1234',
+  password: process.env.DB_PASSWORD || '1234',
   database: 'grupojima',
   autoLoadEntities: true,
-  synchronize: true //Solo quitar comentario si quieren reiniciar la base de datos
+  //synchronize: true //Solo quitar comentario si quieren reiniciar la base de datos
 };
