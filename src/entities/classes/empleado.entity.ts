bedba14d1eb245be0ae089bb8bba508ecb61fe30
DanielPena229185 <daniel.pena229185@potros.itson.edu.mx>
2024-03-10 12:37:50 -0700

    Ya hay herencia de entidades
    Herencia de Empleado es un repartidor y además se creó los enums para los tipos de usuarios

--- src/entities/classes/empleado.entity.ts
+++ src/entities/classes/empleado.entity.ts
@@ -1,14 +1,37 @@
-import { Entity, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, ManyToMany, JoinTable, Column, PrimaryGeneratedColumn, TableInheritance } from 'typeorm';
 import { Tortilleria } from './tortilleria.entity';
-import { Usuario } from './usuario-entity';
+import { UsuarioRol } from '../enums/usuario-roles-enum';
 
 @Entity({ name: 'empleados', schema: 'public' })
-export class Empleado extends Usuario {
+@TableInheritance({ column: {
+  name: 'tipo',
+  enum: UsuarioRol
+} })
+export class Empleado {
+
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: string;
+
+  @Column({ name: 'codigo_empleado', unique: true, nullable: false })
+  codigoEmpleado: number;
+
+  @Column({ name: 'telefono', unique: true, nullable: false })
+  telefono: string;
+
+  @Column({ name: 'nombres', nullable: false })
+  nombres: string;
+
+  @Column({ name: 'apellidos', nullable: false })
+  apellidos: string;
+
+  @Column({ name: 'tipo', nullable: false })
+  tipo: UsuarioRol;
+
   @ManyToMany(() => Tortilleria, (tortilleria) => tortilleria.empleados)
-  @JoinTable({ 
+  @JoinTable({
     name: 'empleados_tortillerias',
     joinColumn: { name: 'empleado_id' },
-    inverseJoinColumn: { name: 'tortilleria_id' }, 
+    inverseJoinColumn: { name: 'tortilleria_id' },
   })
   tortillerias: Tortilleria[];
 }
