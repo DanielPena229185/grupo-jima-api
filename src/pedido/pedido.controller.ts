e9fbe6ab792310422185de4860e6475f286876c0
DanielPena229185 <daniel.pena229185@potros.itson.edu.mx>
2024-03-15 15:29:56 -0700

    Probar negocio
    Aún faltan hacer pruebas

--- src/pedido/pedido.controller.ts
+++ src/pedido/pedido.controller.ts
@@ -4,26 +4,27 @@ import { CrearPedidoDTO } from './input-dtos/crear-pedido-dto';
 import { ObtenerPedidosByTiendaId } from './input-dtos/obtener-pedidos-por-tienda-id-dto';
 import { ObtenerPedidoById } from './input-dtos/obtener-pedido-by-id-dto';
 import { Pedido } from 'src/entities/classes/pedido.entity';
+import { ProductoNegocio } from './producto-negocio';
 
 @Controller('pedido')
 export class PedidoController {
-  constructor(private readonly pedidoService: PedidoService) {}
+  constructor(private readonly productoNegocio: ProductoNegocio) {}
 
   @Get('/tienda/:tiendaId')
   async getPedidosPorTienda(
     @Param() tiendaId: ObtenerPedidosByTiendaId,
   ): Promise<Pedido[]> {
-    return this.pedidoService.getPedidosByTiendaId();
+    return //this.productoNegocio.getPedidosByTiendaId();
   }
 
   // TODO agregar /pedido/:idPedido
   @Get('/:pedidoId')
   async getPedidoPorId(@Param() pedidoId: ObtenerPedidoById): Promise<Pedido> {
-    return this.pedidoService.getPedidoById();
+    return //this.productoNegocio.getPedidoById();
   }
 
   @Post()
   async postPedido(@Body() body: CrearPedidoDTO): Promise<Pedido> {
-    return await this.pedidoService.crearPedido(body);
+    return await this.productoNegocio.crearPedido(body);
   }
 }
---
+++ src/pedido/producto-negocio.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@nestjs/common";
+import { CrearPedidoDTO } from "./input-dtos/crear-pedido-dto";
+import { Pedido } from "src/entities";
+import { PedidoService } from "./pedido.service";
+import { NegocioException } from "src/utils/exceptions/negocio-exception";
+
+@Injectable()
+export class ProductoNegocio {
+
+    constructor(private readonly pedidoService: PedidoService) { }
+
+    async crearPedido(pedido: CrearPedidoDTO): Promise<Pedido> {
+        this.crearPedidoValidaciones(pedido);
+        return this.pedidoService.crearPedido(pedido);
+    }
+
+    private crearPedidoValidaciones(pedido: CrearPedidoDTO) {
+        if (!pedido.tiendaId || pedido.tiendaId.length === 0) {
+            throw new NegocioException('Por favor, selecciona una tienda para atender este pedido.');
+        }
+        if (!pedido.paquetes || pedido.paquetes.length === 0) {
+            throw new NegocioException('Debe haber al menos un paquete en el pedido.');
+        }
+        if(pedido.paquetes || pedido.paquetes.length > 0){
+            pedido.paquetes.some(paquete => {
+                if (!paquete.productoId) {
+                    throw new NegocioException('Debe seleccionar algún producto para el paquete.');
+                }
+                if (paquete.cantidad < 1) {
+                    throw new NegocioException('Los paquetes del pedido deben contener al menos una unidad.');
+                }
+            })
+        }
+    }
+    
+}
